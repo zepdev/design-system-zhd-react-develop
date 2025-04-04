4db3ed12143fc411f5fe0e813645d190391f1186
FarukBloomteq <faruk.smajlovic@bloomteq.com>
2025-04-04 18:25:45 +0200

    fix: Export small hero

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 import '@zepdev/figma-design-tokens/tailwind/design-tokens.preset';
 export * from '../interfaces/global-variants';
 export * from './SH-Corp';
+export * from './SmallHero';
 export * from './Teaser-simple';
 export * from './USP';
 export * from './accrodion-pattern';
