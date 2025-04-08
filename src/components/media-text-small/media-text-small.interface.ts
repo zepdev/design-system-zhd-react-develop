e6d704672e5b15a69620be7d08bec4526c01a41d
Ram Thapa <ramkthapa7@gmail.com>
2025-04-08 20:41:55 +0200

    fix: removed unused description

--- src/components/media-text-small/media-text-small.interface.ts
+++ src/components/media-text-small/media-text-small.interface.ts
@@ -2,7 +2,6 @@ import { BlocksContent } from "@strapi/blocks-react-renderer";
 
 export interface MediaTextSmallProps {
     headline: string;
-    description?: string;
     orderedLists?: string[];
     content?: BlocksContent;
     imageSrc: string;
