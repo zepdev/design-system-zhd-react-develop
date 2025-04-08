18f2d45e695ab19308bc0afebb02f1e0f45b5a79
Ram Thapa <ramkthapa7@gmail.com>
2025-04-08 21:31:21 +0200

    fix: removed unused variables

--- src/components/media-text-small/media-text-small.interface.ts
+++ src/components/media-text-small/media-text-small.interface.ts
@@ -2,7 +2,6 @@ import { BlocksContent } from "@strapi/blocks-react-renderer";
 
 export interface MediaTextSmallProps {
     headline: string;
-    orderedLists?: string[];
     content?: BlocksContent;
     imageSrc: string;
     imageAlt?: string;
