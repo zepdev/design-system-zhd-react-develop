dc00a2f34a52c33f97802c340e32de186c292514
FarukBloomteq <faruk.smajlovic@bloomteq.com>
2025-04-17 10:09:47 +0200

    fix: Anchor nav

--- src/components/anchor-navigation-card/AnchorNavigationCard.test.tsx
+++ src/components/anchor-navigation-card/AnchorNavigationCard.test.tsx
@@ -1,17 +1,9 @@
 import { render } from '@testing-library/react';
 import { AnchorNavigationCard } from './AnchorNavigationCard';
-import { GlobalVariants } from '../../interfaces/global-variants';
 
 describe('Anchor navigation card component', () => {
   it('should render an anchor navigation card component', () => {
-    const Child = (
-      <AnchorNavigationCard
-        headline={'Gasmotoren Baureihe CG132 B (400 - 1.000 kW)'}
-        linkLabel="Mehr zum Gasmotor"
-        variant={GlobalVariants.Zps}
-        scrollToIdLink=""
-      />
-    );
+    const Child = <AnchorNavigationCard linkLabel="Mehr zum Gasmotor" scrollToIdLink="" />;
     const { getByTestId } = render(Child);
     const anchorCardElement = getByTestId('zep-anchor-navigation-card');
     const headlineElement = getByTestId('anchor-navigation-card-headline');
--- src/components/anchor-navigation-card/AnchorNavigationCard.tsx
+++ src/components/anchor-navigation-card/AnchorNavigationCard.tsx
@@ -1,74 +1,25 @@
-import { FunctionalIcon } from '@zepdev/design-system-component-library-react';
-import { cva } from 'class-variance-authority';
+import clsx from 'clsx';
 import React from 'react';
-import { twMerge } from 'tailwind-merge';
 import { AnchorNavigationCardProps } from './anchor-navigation-card.interface';
-import { GlobalVariants } from '../../interfaces/global-variants';
-
-export const anchorCardVariants = {
-  [GlobalVariants.Zps]: ['zep-bg-primary-default'],
-  [GlobalVariants.Cat]: ['zep-bg-neutral-dark-default'],
-};
-export const anchorNavigationCardCva = cva(
-  [
-    `zep-w-full
-    zep-min-w-[290px]
-    zep-max-w-[290px]
-    sm:zep-min-w-[290px]
-    sm:zep-max-w-[352px]
-    md:zep-min-w-[290px]
-    md:zep-max-w-[435px]
-    lg:zep-min-w-[300px]
-    lg:zep-max-w-[700px]
-    zep-p-1.5 
-    md:zep-p-2 
-    lg:zep-p-2.5`,
-  ],
-  {
-    variants: {
-      variant: anchorCardVariants,
-    },
-    defaultVariants: {
-      variant: GlobalVariants.Zps,
-    },
-  },
-);
 
 export const AnchorNavigationCard: React.FC<AnchorNavigationCardProps> = ({
-  headline,
   linkLabel,
   scrollToIdLink,
-  variant,
   className,
   ...props
 }: AnchorNavigationCardProps) => {
   return (
-    <div
-      data-testid="zep-anchor-navigation-card"
-      className={twMerge(
-        anchorNavigationCardCva({ variant }),
-        className,
-        'zep-flex zep-flex-col zep-gap-1.5 zep-@container-normal',
+    <div data-testid="zep-anchor-navigation-card" className={clsx(className, 'zep-min-w-max')} {...props}>
+      {linkLabel && (
+        <a href={`#${scrollToIdLink}`}>
+          <h4
+            className="zep-text-typography-dark-100 zep-hyphens-auto zep-break-normal hover:zep-text-typography-dark-70/70"
+            data-testid="anchor-navigation-card-headline"
+          >
+            {linkLabel}
+          </h4>
+        </a>
       )}
-      {...props}
-    >
-      {headline && (
-        <h3
-          className="zep-typography-headlineSM-fluid-cqi zep-text-typography-light-100 zep-hyphens-auto zep-break-normal"
-          data-testid="anchor-navigation-card-headline"
-        >
-          {headline}
-        </h3>
-      )}
-      <a href={`#${scrollToIdLink}`}>
-        <div className="zep-flex zep-items-center zep-gap-0.5">
-          <p className="zep-text-secondary-default hover:zep-underline hover:zep-transition-all">{linkLabel}</p>
-          <FunctionalIcon
-            name={'arrow-long-down'}
-            className="zep-stroke-secondary-default zep-fill-secondary-default zep-w-1"
-          />
-        </div>
-      </a>
     </div>
   );
 };
