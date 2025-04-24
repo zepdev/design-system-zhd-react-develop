5eb23e8b69dec898b672a3d9d69e035bd5aaaf0a
FarukBloomteq <faruk.smajlovic@bloomteq.com>
2025-04-24 12:20:51 +0200

    fix: api client types

--- src/components/card-media-pattern/CardMediaPattern.interface.ts
+++ src/components/card-media-pattern/CardMediaPattern.interface.ts
@@ -1,11 +1,9 @@
 import { CardMediaProps } from '../card-media-component';
 import { HeaderLongProps } from '../header-long';
-import { GlobalVariants } from '../../interfaces/global-variants';
 
 export interface CardMediaPatternProps extends Omit<HeaderLongProps, 'variant'> {
-  cards: CardMediaProps[],
-  className?: string,
-  variant?: GlobalVariants,
-  imageOrientation?: 'horizontal' | 'vertical',
-  id?: string,
+  cards: CardMediaProps[];
+  className?: string;
+  imageOrientation?: 'horizontal' | 'vertical' | 'none';
+  id?: string;
 }
