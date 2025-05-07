23ba5e1ae31de6dfa3f85f55c4387b427c88b216
FarukBloomteq <faruk.smajlovic@bloomteq.com>
2025-05-07 14:06:09 +0200

    fix: build errors

--- src/components/USP/Usp.test.tsx
+++ src/components/USP/Usp.test.tsx
@@ -3,7 +3,7 @@ import { USP } from './Usp';
 
 describe('USP component', () => {
   it('should render', () => {
-    const { getByTestId } = render(<USP iconName="hook" count={'50'} headline="test" description="random text"></USP>);
+    const { getByTestId } = render(<USP count={'50'} headline="test" description="random text"></USP>);
     const USPElement = getByTestId('zep-usp');
     // Deleting the line in the test since we don't need the px-1 class anymore
     expect(USPElement).toBeInTheDocument();
