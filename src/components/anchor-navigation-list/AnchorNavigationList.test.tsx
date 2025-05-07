b51b95145147e0808e790702c15a9ce5148a1f59
nazih <a7mednazih1994@gmail.com>
2025-05-07 09:07:09 +0200

    feat: add new anchor navigation with new colors

--- src/components/anchor-navigation-list/AnchorNavigationList.test.tsx
+++ src/components/anchor-navigation-list/AnchorNavigationList.test.tsx
@@ -1,16 +1,17 @@
 import { render } from '@testing-library/react';
 import { AnchorNavigationList } from './AnchorNavigationList';
+import { GlobalVariants } from '../../interfaces/global-variants';
 
 // Mock ResizeObserver after imports
 global.ResizeObserver = class {
   observe() {}
   unobserve() {}
   disconnect() {}
-};
+}
 
 describe('Anchor navigation list component', () => {
   it('should render an anchor navigation list component', () => {
-    const Child = <AnchorNavigationList navigationCards={[]} />;
+    const Child = <AnchorNavigationList navigationCards={[]} variant={GlobalVariants.Zps} />;
     const { getByTestId } = render(Child);
     const anchorCardElement = getByTestId('zep-anchor-navigation-list');
     expect(anchorCardElement).toBeInTheDocument();
