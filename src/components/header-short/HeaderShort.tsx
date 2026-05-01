8d1837121dd5df4c4ac47ea07ddfa143a3edcc70
Duhkha <ismar.kovacevic@stu.ibu.edu.ba>
2026-05-01 18:44:40 +0200

    feat: add disableBottomPadding prop to HeaderShort component

--- src/components/header-short/HeaderShort.tsx
+++ src/components/header-short/HeaderShort.tsx
@@ -4,17 +4,22 @@ import { HeaderShortComponent } from '../header-short-component';
 import { Layout } from '../layout';
 import { HeaderShortProps } from './headershort.interface';
 
-export const HeaderShort: React.FC<HeaderShortProps> = ({ className, backgroundVariant = 'white', ...props }) => {
+export const HeaderShort: React.FC<HeaderShortProps> = ({
+  className,
+  backgroundVariant = 'white',
+  disableBottomPadding = false,
+  ...props
+}) => {
   const colSpanByArrow = 'md:zep-w-[35%]';
 
   const wrapperBg = backgroundVariant === 'gray' ? 'zep-bg-background-medium' : undefined;
 
+  const paddingClasses = disableBottomPadding
+    ? ['zep-pt-3', 'sm:zep-pt-4', 'md:zep-pt-5']
+    : ['zep-py-3', 'sm:zep-py-4', 'md:zep-py-5'];
+
   return (
-    <Layout
-      className={clsx(className, 'zep-py-3', 'sm:zep-py-4', 'md:zep-py-5')}
-      wrapperClassname={wrapperBg}
-      testId="header-short"
-    >
+    <Layout className={clsx(className, ...paddingClasses)} wrapperClassname={wrapperBg} testId="header-short">
       <HeaderShortComponent {...props} headlineSize="xl" className={clsx('zep-w-full', colSpanByArrow)} />
     </Layout>
   );
